843f68ffac793badaf59f888e5397f08bc84a2a2
suyesh Bhandari <suyeshb@gmail.com>
2013-08-27 11:11:05 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trollmemaybe::Application.config.secret_key_base = 'ed15eda9e73ae0e65d1ed6ffb0f0715f50a802d3dde63eed23b07c1c803bc91de1fca61884c3c7beff43672f224f3f7e93d54a7b6cc217ec30900a52e99314a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trollmemaybe::Application.config.session_store :cookie_store, key: '_trollmemaybe_session'
